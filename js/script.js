31996f620898b404d04e4a97d286ef6062a9ee5d
jimmyw32234 <jim.w32234@gmail.com>
2020-02-26 23:06:51 +0000

    mymedsge

--- js/script.js
+++ js/script.js
@@ -1,11 +1,56 @@
 let songs = []
+let artists = []
+let lengths = []
+let images = []
+let links = []
 
 function showSong(){
-songs.forEach(function(song) {
-$("#songs").append("<p>" + song + "<p>");
+    songs.forEach(function(song) {
+        $("#songs").append("<div>" + song + "</div>");
     });
 }
 
-$("#add").click(function() {
-    showSong();
-});
+
+function showLength(){
+    lengths.forEach(function(length) {
+        $("#lengths").append("<div>" + length + "</div>");
+    });
+}
+
+function showArtist(){
+    artists.forEach(function(artist) {
+        $("#artists").append("<div>" + artist + "</div>");
+    });
+}
+
+function showImage() {
+    images.forEach(function(image) {
+        $("#images").append("<div>" + image + "</div>");
+    });
+}
+
+function showLink() {
+    links.forEach(function(link) {
+        $("#links").append("<div>" + link + "</div>");
+    })
+}
+
+function songInfo(item1, item2) {
+let item = $(item2).val();
+(item1).push(item);
+
+}
+
+$("#add").click(function(){    
+        showSong();
+        showArtist();
+        showLength();
+        showImage();
+        showLink();
+        songInfo(songs, "#song");
+        songInfo(artists, "#artist");
+        songInfo(lengths, "#length");
+        songInfo(images, "#image");
+        songInfo(links, "#link");
+    }
+)
